068583cf2c95f06768346d8bf0700f06bc84afb0
Hey R You Are <55276676+Abab-bk@users.noreply.github.com>
2025-01-01 21:15:07 +0800

    refactor: Remove Server

---
+++ Src/Scripts/Base/CharacterEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Game.Scripts.Classes;
+using Game.Scripts.Interfaces;
+using Godot;
+
+namespace Game.Scripts.Base;
+
+public partial class GameEntity : CharacterBody2D, IGameEntity
+{
+    public List<Component> Components { get; } = new List<Component>();
+}
